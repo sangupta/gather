c3fb2104f0f1dc7f051d3a7d63d3c896359f477d
Sandeep Gupta <sangupta@adobe.com>
2017-06-05 15:28:27 +0530

    More units

--- src/test/java/com/sangupta/gather/TestGather.java
+++ src/test/java/com/sangupta/gather/TestGather.java
@@ -2,7 +2,6 @@ package com.sangupta.gather;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,39 +10,59 @@ public class TestGather {
 	
 	@Test
 	public void testGather() {
+		final List<Worker> workers = getWorkers();
+		
 		Gather query = Gather.where("name").is("sandeep");
 		
 		Assert.assertEquals(0, query.count(null));
 		Assert.assertEquals(0, query.count(new ArrayList<>()));
-		Assert.assertEquals(0, query.count(getWorkers()));
+		Assert.assertEquals(0, query.count(workers));
 
 		query = Gather.where("name").is("sandeep gupta");
-		Assert.assertEquals(0, query.count(getWorkers()));
+		Assert.assertEquals(0, query.count(workers));
 
 	
 		query = Gather.where("name").isIgnoreCase("sandeep gupta");
-		Assert.assertEquals(1, query.count(getWorkers()));
+		Assert.assertEquals(1, query.count(workers));
 		
 		query = Gather.where("name").like("*gupta");
-		Assert.assertEquals(0, query.count(getWorkers()));
+		Assert.assertEquals(0, query.count(workers));
 		
 		query = Gather.where("name").like("S*Gupta");
-		Assert.assertEquals(2, query.count(getWorkers()));
+		Assert.assertEquals(2, query.count(workers));
 		
 		query = query.and("active").is(true);
-		Assert.assertEquals(1, query.count(getWorkers()));
+		Assert.assertEquals(1, query.count(workers));
 		
 		query = Gather.where("name").like("S*Gupta").and("active").is(false);
-		Assert.assertEquals(1, query.count(getWorkers()));
+		Assert.assertEquals(1, query.count(workers));
 		
-//		query = Gather.where("salary").greaterThan(40);
-//		Assert.assertEquals(3, query.count(getWorkers()));
-//		
-//		query = Gather.where("salary").greaterThan(10);
-//		Assert.assertEquals(4, query.count(getWorkers()));
-//		
-//		query = Gather.where("salary").greaterThan(100);
-//		Assert.assertEquals(0, query.count(getWorkers()));
+		query = Gather.where("salary").greaterThan(40l);
+		Assert.assertEquals(3, query.count(workers));
+		
+		query = Gather.where("salary").greaterThan(50l);
+		Assert.assertEquals(1, query.count(workers));
+		
+		query = Gather.where("salary").greaterThanOrEquals(50l);
+		Assert.assertEquals(3, query.count(workers));
+		
+		query = Gather.where("salary").greaterThanOrEquals(50l).and("active").is(true);
+		Assert.assertEquals(1, query.count(workers));
+		
+		query = Gather.where("salary").greaterThanOrEquals(50l).and("active").is(false);
+		Assert.assertEquals(2, query.count(workers));
+		
+		query = Gather.where("salary").greaterThan(10l);
+		Assert.assertEquals(4, query.count(workers));
+		
+		query = Gather.where("salary").greaterThan(100l);
+		Assert.assertEquals(0, query.count(workers));
+		
+		query = Gather.where("salary").lessThan(50l);
+		Assert.assertEquals(1, query.count(workers));
+		
+		query = Gather.where("salary").lessThanOrEquals(50l);
+		Assert.assertEquals(3, query.count(workers));
 	}
 	
 	private List<Worker> getWorkers() {
@@ -65,13 +84,13 @@ public class TestGather {
 		
 		private boolean active;
 		
-		private AtomicLong salary;
+		private long salary;
 		
 		public Worker(String name, int age, boolean active, long salary) {
 			this.name = name;
 			this.age = age;
 			this.active = active;
-			this.salary = new AtomicLong(salary);
+			this.salary = salary;
 		}
 		
 	}
